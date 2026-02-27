987691da2a07ee9443d71ee4bff6b7f90b455204
architashinde <architashinde45@gmail.com>
2026-02-27 11:51:38 +0530

    updated .sol

--- contracts-backend/contracts/DocChain.sol
+++ contracts-backend/contracts/DocChain.sol
@@ -5,37 +5,45 @@ contract DocChain {
 
     struct Document {
         string hash;
+        string ipfsCID;
+        string docType;
         address owner;
         uint256 timestamp;
     }
 
     mapping(string => Document) public documents;
 
-    event DocumentStored(string hash, address owner);
+    event DocumentStored(string hash, address owner, string ipfsCID, string docType);
 
-    function storeDocument(string memory _hash) public {
-        require(documents[_hash].timestamp == 0, "Already exists");
+    function storeDocument(
+        string memory _hash,
+        string memory _ipfsCID,
+        string memory _docType
+    ) public {
+        require(documents[_hash].timestamp == 0, "Document already exists on blockchain");
 
         documents[_hash] = Document(
             _hash,
+            _ipfsCID,
+            _docType,
             msg.sender,
             block.timestamp
         );
 
-        emit DocumentStored(_hash, msg.sender);
+        emit DocumentStored(_hash, msg.sender, _ipfsCID, _docType);
     }
 
     function verifyDocument(string memory _hash)
         public
         view
-        returns (bool, address, uint256)
+        returns (bool, address, uint256, string memory, string memory)
     {
         Document memory doc = documents[_hash];
 
         if (doc.timestamp == 0) {
-            return (false, address(0), 0);
+            return (false, address(0), 0, "", "");
         }
 
-        return (true, doc.owner, doc.timestamp);
+        return (true, doc.owner, doc.timestamp, doc.ipfsCID, doc.docType);
     }
 }
